ccd580c0c006b8dce8c05086105f42e691cc78e0
Darren <darren.q@gmail.com>
2015-09-01 15:51:51 -0400

    replaced ~ paths with HOME var

--- github_setup.sh
+++ github_setup.sh
@@ -6,14 +6,17 @@ GIT_EMAIL='darren.q@gmail.com'
 GITHUB_USER='darren-trx'
 GITHUB_REPO_WEB='https://github.com/darren-trx/bootstrap.git'
 GITHUB_REPO_SSH='git@github.com:darren-trx/bootstrap.git'
-GITHUB_REPO_DIR=~/bootstrap
-GITHUB_PRIV_KEY=~/.ssh/github_rsa
+GITHUB_REPO_DIR="${HOME}/bootstrap"
+GITHUB_PRIV_KEY="${HOME}/.ssh/github_rsa"
 GITHUB_AUTH_URL='https://api.github.com/user/keys'
 
 # this script assumes it has been downloaded using:
 # git clone <GITHUB_REPO_WEB> <GITHUB_REPO_DIR>
 cd "${GITHUB_REPO_DIR}"
-if [ $? -ne 0 ]; then echo "Could not cd into ${GITHUB_REPO_DIR}" && exit 1; fi
+if [ $? -ne 0 ]; then
+  echo "Could not cd into ${GITHUB_REPO_DIR}"
+  exit 1
+fi
 
 # generate new public/private key pair
 ssh-keygen -t rsa -b 4096 -f "${GITHUB_PRIV_KEY}" -C "${IDENT_INFO}"
@@ -27,13 +30,13 @@ curl -s -d "${GITHUB_AUTH_JSON}" "${GITHUB_AUTH_URL}" -u "${GITHUB_USER}" | grep
 cd "${GITHUB_REPO_DIR}"
 
 # if ~/.gitconfig already exists, just output a message
-if [ -f ~/.gitconfig ]; then
-  echo '~/.gitconfig already exists'
+if [ -f "${HOME}/.gitconfig" ]; then
+  echo "${HOME}/.gitconfig already exists"
 # if ~/.gitconfig does not exist, symlink the one
 # in this directory or create a very basic one
 else
   if [ -f "${GITHUB_REPO_DIR}/dotfiles/.gitconfig" ]; then
-    ln -s "${GITHUB_REPO_DIR}/dotfiles/.gitconfig" ~/.gitconfig
+    ln -s "${GITHUB_REPO_DIR}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
   else
     git config --global user.name "${GIT_USER}"
     git config --global user.email "${GIT_EMAIL}"
@@ -45,20 +48,15 @@ fi
 git remote set-url --push origin "${GITHUB_REPO_SSH}" 
 
 # start ssh agent
-SSH_ENV=~/.ssh/env
-/usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+SSH_ENV="${HOME}/.ssh/env"
+ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
 chmod 600 "${SSH_ENV}"
-. "${SSH_ENV}" > /dev/null
-
-# add the private key
-ssh-add "${GITHUB_PRIV_KEY}"
-
-# list currently loaded ssh keys
-echo ""
-ssh-add -l
 
 # list current git repo remotes
-echo ""
+echo
+echo "Github remote repos:"
 git remote -v
-
-echo 'Now run . ~/.ssh/env'
+echo
+echo 'Now run:'
+echo ' . ~/.ssh/env'
+echo ' ssh-add ~/.ssh/github_rsa'
