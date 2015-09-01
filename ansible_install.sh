f3da8c166e328f753766dd3063a9f69aae3c24d6
Darren <darren.q@gmail.com>
2015-09-01 15:00:10 -0400

    first commit

---
+++ ansible_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt-get install -y sshpass
+
+# install pip if necessary
+[ "$(which pip)" ] || sudo ./pip_install.sh
+
+sudo pip install paramiko PyYAML Jinja2 httplib2 ansible
+
+ansible --version
