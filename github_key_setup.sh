1ee1fbed8b4b9a0954ea9eff6f567ac7625b7c97
Darren <darren.q@gmail.com>
2015-09-03 12:17:51 -0400

    various improvements

--- github_key_setup.sh
+++ github_key_setup.sh
@@ -18,25 +18,33 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# generate new public/private key pair
+# generate new public/private key pair (RSA 4096)
 ssh-keygen -t rsa -b 4096 -f "${GITHUB_PRIV_KEY}" -C "${IDENT_INFO}"
 
 # use github api to authorize the public key
 GITHUB_PUB_KEY=`cat "${GITHUB_PRIV_KEY}.pub"`
 GITHUB_AUTH_JSON="{\"title\": \"${IDENT_INFO}\", \"key\": \"${GITHUB_PUB_KEY}\"}"
 
-curl -s -d "${GITHUB_AUTH_JSON}" "${GITHUB_AUTH_URL}" -u "${GITHUB_USER}" | grep message
+# -s silent, -u Server user, -d Data to send (POST)
+curl -s -u "${GITHUB_USER}" -d "${GITHUB_AUTH_JSON}" "${GITHUB_AUTH_URL}" | grep message
 
-cd "${GITHUB_REPO_DIR}"
+# if ~/.gitconfig already exists, prompt user if it should be deleted
+if [ -e "${HOME}/.gitconfig" ]; then
+  read -n1 -t10 -p "${HOME}/.gitconfig already exists, delete it? (y/n): " DEL_GIT_CFG
+  echo
+  if [ "${DEL_GIT_CFG}" == "y" ]; then 
+    rm -fv "${HOME}/.gitconfig"
+  else
+    echo "Existing .gitconfig left as is"
+  fi
+fi
 
-# if ~/.gitconfig already exists, just output a message
-if [ -f "${HOME}/.gitconfig" ]; then
-  echo "${HOME}/.gitconfig already exists"
-# if ~/.gitconfig does not exist, symlink the one
-# in this directory or create a very basic one
-else
+# if ~/.gitconfig does not exist, either:
+# (A) symlink the one from the git repo dotfiles dir, or
+# (B) create a new one with only the bare necessities
+if [ ! -e "${HOME}/.gitconfig" ]; then
   if [ -f "${GITHUB_REPO_DIR}/dotfiles/.gitconfig" ]; then
-    ln -s "${GITHUB_REPO_DIR}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
+    ln -v -s "${GITHUB_REPO_DIR}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
   else
     git config --global user.name "${GIT_USER}"
     git config --global user.email "${GIT_EMAIL}"
@@ -44,13 +52,16 @@ else
   fi
 fi
 
-# replace https remote with ssh one
+# set git remote origins to ssh url
+git remote set-url origin "${GITHUB_REPO_SSH}"
 git remote set-url --push origin "${GITHUB_REPO_SSH}" 
 
-# start ssh agent
+# create an ssh agent env file if there isnt one already
 SSH_ENV="${HOME}/.ssh/env"
-ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-chmod 600 "${SSH_ENV}"
+if [ ! -f "${SSH_ENV}" ]; then
+  ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+  chmod 600 "${SSH_ENV}"
+fi
 
 # list current git repo remotes
 echo
@@ -58,5 +69,5 @@ echo "Github remote repos:"
 git remote -v
 echo
 echo 'Now run:'
-echo ' . ~/.ssh/env'
-echo ' ssh-add ~/.ssh/github_rsa'
+echo '1) . ~/.ssh/env'
+echo '2) ssh-add ~/.ssh/github_rsa'
