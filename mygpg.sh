5c188f9384f4060ea19191920d83fffa1a208f31
Darren <darren.q@gmail.com>
2015-09-09 16:22:11 -0400

    refactored to run directly from path

--- mygpg.sh
+++ mygpg.sh
@@ -1,15 +1,39 @@
 #!/bin/sh
 
-MY_GPG="darren.q@gmail.com"
+MYGPG_EMAIL="darren.q@gmail.com"
+
+MYGPG_SRC="${HOME}/bootstrap/mygpg.sh"
+MYGPG_BIN="/usr/local/bin/mygpg"
+THIS_FILE=$(readlink -f "$0")
+
+if [ "${THIS_FILE}" != "${MYGPG_BIN}" ]; then
+  sudo cp -v "${MYGPG_SRC}" "${MYGPG_BIN}"
+  exit 1
+fi
 
 mygpg_fp_key() {
-  gpg --fingerprint $MY_GPG | awk -F'=' '/Key fingerprint/ {print $2}' | sed 's/ //g'
+  gpg --fingerprint $MYGPG_EMAIL 2>/dev/null | awk -F'=' '/Key fingerprint/ {print $2}' | sed 's/ //g'
+}
+mygpg_clear_at() {
+  ATQ="$(atq | cut -f1)"
+  if [ "$ATQ" != "" ]; then
+    for i in "$ATQ"; do
+      at -c $i | grep -qow "$MYGPG_BIN"
+      [ $? -gt 0 ] || atrm $i &>/dev/null
+    done
+  fi
 }
 mygpg_del_key() {
-  echo "Deleting secret key for $MY_GPG $(mygpg_fp_key)"
-  gpg --delete-secret-keys --batch --yes "$(mygpg_fp_key)"
-  echo "Deleting public key for $MY_GPG"
-  gpg --delete-keys --batch --yes $MY_GPG
+  MYGPG_FP="$(mygpg_fp_key)"
+  if [ "$MYGPG_FP" != "" ]; then
+    echo "Deleting secret key for $MYGPG_EMAIL $MYGPG_FP"
+    gpg --delete-secret-keys --batch --yes "$MYGPG_FP"
+    echo "Deleting public key for $MYGPG_EMAIL"
+    gpg --delete-keys --batch --yes $MYGPG_EMAIL
+  else
+    echo "No key(s) loaded for $MYGPG_EMAIL"
+  fi
+  mygpg_clear_at
 }
 mygpg_add_key() { 
   echo "$1" | egrep -q "\.gpg$|\.asc$"
@@ -21,18 +45,15 @@ mygpg_add_key() {
 
   # trust this key ultimately (6)
   echo "$(mygpg_fp_key):6:" | gpg --import-ownertrust
-
-  # flush the key after 5 hours, deleting any other gpg-del jobs first
-  for i in "$(atq | cut -f1)"; do
-    at -c $i | grep -qow "$HOME/.mygpg.sh"
-    [ $? -gt 0 ] || atrm $i &>/dev/null
-  done
-  echo "$HOME/.mygpg.sh -d" | at now + 300 minutes
+  
+  # flush the key after 5 hours, deleting any existing at jobs first
+  mygpg_clear_at
+  echo "$MYGPG_BIN -d" | at now + 300 minutes
 }
 
 case "$1" in
   "-e")
-    gpg --encrypt --armor -r $MY_GPG "$2";;
+    gpg --encrypt --armor -r $MYGPG_EMAIL "$2";;
   "-s")
     gpg --symmetric --cipher-algo AES256 --armor --verbose "$2";;
   "-r")
@@ -46,5 +67,11 @@ case "$1" in
   "-l")
     gpg --list-keys && gpg --list-secret-keys;;
   *)
-    gpg "$@";;
+    echo "  My GPG Key: $MYGPG_EMAIL $(mygpg_fp_key)
+      -e  asymmetrically encrypt <file> using my gpg key
+      -s  symetrically encrypt <file> using AES256
+      -r  run <file> with bash
+      -a  add gpg key (encrypted or not)
+      -d  delete my gpg key"
+    ;;
 esac
