3d2b82d2a0ab55135972674f4e8aa6664713c792
Darren <darren.q@gmail.com>
2015-09-09 14:10:22 -0400

    moved git subtree add/pull into --refresh

--- vim_plugins.sh
+++ vim_plugins.sh
@@ -13,34 +13,31 @@ plugin["vim-fugitive"]="https://github.com/tpope/vim-fugitive"
 plugin["vim-gnupg"]="https://github.com/jamessan/vim-gnupg"
 
 VIM_HOME="${HOME}/.vim"
-VIM_PLUGINS="${HOME}/vim-plugins"
+VIM_PLUGINS="${HOME}/.vim-plugins"
 
-[ -d "${VIM_HOME}" ] || mkdir -v "${VIM_HOME}"
+# create ~/.vim if nothing at all exists
+[ -e "${VIM_HOME}" ] || mkdir -v "${VIM_HOME}"
+
+# clone vim plugin repo into ~/.vim-plugins
 [ -d "${VIM_PLUGINS}" ] || git clone "${VIM_PLUGINS_SRC}" "${VIM_PLUGINS}"
-[ -d "${VIM_PLUGINS}/bundle" ] || mkdir -v "${VIM_PLUGINS}/bundle"
 
-cd "${VIM_PLUGINS}"
+# refresh the vim plugin subtrees from their original repos
+if [ "$1" == "--refresh" ]; then
+  cd "${VIM_PLUGINS}"
 
-for p in "${!plugin[@]}"
-do
-  if [ ! -d "bundle/${p}" ]; then
-    git subtree add --prefix "bundle/${p}" "${plugin[$p]}" master --squash
-  else
-    git subtree pull --prefix "bundle/${p}" "${plugin[$p]}" master --squash
-  fi
-done
-
-if [ -L "${VIM_HOME}/autoload" ]; then
-  rm -v "${VIM_HOME}/autoload"
-elif [ -e "${VIM_HOME}/autoload" ]; then
-  mv -vf "${VIM_HOME}/autoload" "${VIM_HOME}/autoload.bak"
+  for p in "${!plugin[@]}"
+  do
+    if [ ! -d "bundle/${p}" ]; then
+      git subtree add --prefix "bundle/${p}" "${plugin[$p]}" master --squash
+    else
+      git subtree pull --prefix "bundle/${p}" "${plugin[$p]}" master --squash
+    fi
+  done
 fi
 
-if [ -L "${VIM_HOME}/bundle" ]; then
-  rm -v "${VIM_HOME}/bundle"
-elif [ -e "${VIM_HOME}/bundle" ]; then
-  mv -vf "${VIM_HOME}/bundle" "${VIM_HOME}/bundle.bak"
-fi
+# delete existing autoload/bundle with extreme prejudice
+[ ! -e "${VIM_HOME}/autoload" ] || rm -rfv "${VIM_HOME}/autoload" 
+[ ! -e "${VIM_HOME}/bundle" ] || rm -rfv "${VIM_HOME}/bundle"
 
 ln -v -s "${VIM_PLUGINS}/bundle/vim-pathogen/autoload" "${VIM_HOME}/autoload"
 ln -v -s "${VIM_PLUGINS}/bundle" "${VIM_HOME}/bundle"
