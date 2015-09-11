5d985fff78d53f4e5a1fbe50f3a5f64763f18093
Darren <darren.q@gmail.com>
2015-09-11 15:49:01 -0400

    replaced pip install with git submodule

---
+++ dotfiles_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# https://github.com/jbernard/dotfiles/
+# https://pypi.python.org/pypi/dotfiles
+# pip install dotfiles  (alternative)
+
+DOTFILES_BIN="${HOME}/bootstrap/dotfiles_setup/bin/dotfiles"
+DOTFILES_REPO="${HOME}/bootstrap/dotfiles"
+DOTFILES_LINK="${HOME}/.dotfilesrc"
+
+DOTFILES_OPTS="$@"
+
+[ -x "${DOTFILES_BIN}" ] || ( echo "${DOTFILES_BIN} does not exist, aborting" && exit 1)
+[ -d "${DOTFILES_REPO}" ] ||  ( echo "${DOTFILES_REPO} does not exist, aborting" && exit 1 )
+
+# delete ~/.dotfilesrc if it already exists
+if [ -e "${DOTFILES_LINK}" ]; then
+  rm -vf "${DOTFILES_LINK}"
+fi
+
+# symlink ~/.dotfilesrc manually initially
+ln -s "${DOTFILES_REPO}/.dotfilesrc" "${DOTFILES_LINK}"
+
+# sync, list, and check dotfiles symlinks
+echo "Syncing dotfiles..."
+$DOTFILES_BIN --sync $DOTFILES_OPTS
+echo
+echo "Currently managed dotfiles:"
+$DOTFILES_BIN --list
+echo
+echo "Checking for broken/unsynced symlinks..."
+$DOTFILES_BIN --check
+
+# Reload bash env files
+source "${HOME}/.bashrc"
+source "${HOME}/.bash_aliases"
+
