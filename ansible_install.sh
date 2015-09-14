c8fe1c01b09c50430bd3bf54810aa1725fd23bff
Darren <darren.q@gmail.com>
2015-09-14 00:13:06 -0400

    ansible pip modules added

--- ansible_install.sh
+++ ansible_install.sh
@@ -5,6 +5,8 @@ sudo apt-get install -y sshpass
 # install pip if necessary
 [ "$(which pip)" ] || sudo ./pip_install.sh
 
-sudo pip install paramiko PyYAML Jinja2 httplib2 ansible
+sudo pip install paramiko PyYAML Jinja2 httplib2 \
+                 dopy passlib credstash \
+                 ansible
 
 ansible --version
