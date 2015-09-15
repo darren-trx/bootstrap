c4ae84b6e0dbf9a713d12b609deeb77bb5ef432f
Darren <darren.q@gmail.com>
2015-09-14 22:05:06 -0400

    removed ansible credstash module

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# sshpass is required for password-based ssh login
 sudo apt-get install -y sshpass
 
 # install pip if necessary
 [ "$(which pip)" ] || sudo ./pip_install.sh
 
-sudo pip install paramiko PyYAML Jinja2 httplib2 \
-                 dopy passlib credstash \
-                 ansible
+# install relevent modules, then ansible itself
+sudo pip install paramiko PyYAML Jinja2 httplib2 dopy passlib ansible
 
 ansible --version
+whereis ansible
