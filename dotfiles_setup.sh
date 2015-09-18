2e522f1c59e512669bb0d8bc2e944807591cfdc6
Darren <darren.q@gmail.com>
2015-09-17 21:17:05 -0400

    created my own dotfiles script in bash

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,42 +1,79 @@
 #!/bin/bash
 
-# https://github.com/jbernard/dotfiles/
-# https://pypi.python.org/pypi/dotfiles
-# pip install dotfiles  (alternative)
+# YADSWS
+# Yet Another Dotfiles Symlinker, With Color!
 
-DOTFILES_BIN="${HOME}/bootstrap/dotfiles_setup/bin/dotfiles"
-DOTFILES_REPO="${HOME}/bootstrap/dotfiles"
-DOTFILES_LINK="${HOME}/.dotfilesrc"
+DOTFILES_BKP_DIR="${HOME}/dotfiles_backup"
+DOTFILES_GIT_DIR="${HOME}/bootstrap/dotfiles"
 
-DOTFILES_OPTS="$@"
+Red=`tput setaf 1`
+Green=`tput setaf 2`
+Yellow=`tput setaf 3`
+Blue=`tput setaf 4`
+Grey=`tput setaf 8`
+Orange=`tput setaf 9`
+BGreen=`tput setaf 10`
+BYellow=`tput setaf 11`
+BBlue=`tput setaf 12`
+Reset=`tput sgr0`
 
-[ -x "${DOTFILES_BIN}" ] || ( echo "${DOTFILES_BIN} does not exist, aborting" && exit 1)
-[ -d "${DOTFILES_REPO}" ] ||  ( echo "${DOTFILES_REPO} does not exist, aborting" && exit 1 )
+status_msg() {
+  local RESULT=""
+  if [ "$2" ]; then
+    case "$2" in
+      0) RESULT="${Green}OK${Reset}" ;;
+      1) RESULT="${Red}FAIL${Reset}" ;;
+      *) RESULT="${Orange}??${Reset}" ;;
+    esac
+    echo -n " ... ${1} [ ${RESULT} ]"
+  else
+    echo -n " ... ${1}"
+  fi
+}
 
-# delete ~/.dotfilesrc if it already exists
-if [ -e "${DOTFILES_LINK}" ]; then
-  rm -f "${DOTFILES_LINK}"
-fi
+for DF in $( ls -A "${DOTFILES_GIT_DIR}" )
+do
+  echo -n "${Yellow}${DF}${Reset}"
+  RET=0
+  # if the file already exists in ~/
+    # if the existing file is a symlink, just remove it
+    if [ -L "${HOME}/${DF}" ]; then
+      # if the symlink does not point to the right location
+      if [ "$(readlink ${HOME}/${DF})" != "${DOTFILES_GIT_DIR}/${DF}" ]; then
+        rm -f "${HOME}/${DF}"
+        RET=$?
+        status_msg "Deleting existing symlink" "$RET"
+      else
+        # file is a sym link pointing to the right location
+        status_msg "${Grey}Matches existing symlink, skipping${Reset}"
+        RET=2
+      fi
+    # if the existing file is not a symlink, back it up first
+    elif [ -f "${HOME}/${DF}" ] || [ -d "${HOME}/${DF}" ]; then
 
-# backup .bashrc if it is not a symlink
-if [ ! -L "${HOME}/.bashrc" ]; then
-  mv -vf "${HOME}/.bashrc" "${HOME}/.bashrc.bak"
-fi
+      # create backup dir if it does not exist already
+      if [ ! -d "${DOTFILES_BKP_DIR}" ]; then mkdir -p "${DOTFILES_BKP_DIR}"; fi
 
-# symlink ~/.dotfilesrc manually initially
-ln -s "${DOTFILES_REPO}/.dotfilesrc" "${DOTFILES_LINK}"
+      # delete any existing backup of this file/dir
+      if [ -e "${DOTFILES_BKP_DIR}/${DF}" ]; then rm -rf "${DOTFILES_BKP_DIR}/${DF}"; fi
 
-# sync, list, and check dotfiles symlinks
-echo "Syncing dotfiles..."
-$DOTFILES_BIN --sync $DOTFILES_OPTS
-echo
-echo "Currently managed dotfiles:"
-$DOTFILES_BIN --list
-echo
-echo "Checking for broken/unsynced symlinks..."
-$DOTFILES_BIN --check
+      mv -f "${HOME}/${DF}" "${DOTFILES_BKP_DIR}/${DF}"
+      RET=$?
+      status_msg "Backing up file/dir" "$RET"
+    fi
 
-# Reload bash env files
+  # if the file existed in ~/ it should have been moved or deleted
+  # and sym linking should be safe to perform now
+  if [ $RET -eq 0 ]; then
+    ln -s "${DOTFILES_GIT_DIR}/${DF}" "${HOME}/${DF}"
+    RET=$?
+    status_msg "Creating symlink" "$RET"
+  fi
+
+  # line break
+  echo
+
+done
+
+# Reload bashrc automatically
 source "${HOME}/.bashrc"
-source "${HOME}/.bash_aliases"
-
