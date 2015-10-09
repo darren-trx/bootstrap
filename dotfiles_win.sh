e6c9a5b86732b5e50caa591a5652c7666c6f661d
Darren <darren.q@gmail.com>
2015-10-08 23:56:17 -0400

    dotfiles script for windows

---
+++ dotfiles_win.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -x
+
+# dotfiles/.bash_aliases "grep --color" currently screws some scripts up in Windows
+cd ./dotfiles/
+cp -f .bashrc $HOME
+cp -f .bash_ssh_agent $HOME
+cp -f .gitconfig $HOME
+sed -i 's/#\(.* # Windows\)/\1/g' $HOME/.gitconfig
+cp -f .gitignore $HOME
+cp -f .vimrc $HOME
+cp -rf .vim $HOME/vimfiles
+
+cd ../../vim-plugins/bundle
+cp -rvf vim-pathogen/autoload $HOME/vimfiles/
+
+BUNDLE_DIR="$HOME/vimfiles/bundle"
+mkdir "$BUNDLE_DIR"
+cp -rf ctrlp "$BUNDLE_DIR"
+cp -rf undotree "$BUNDLE_DIR"
+cp -rf vim-airline "$BUNDLE_DIR"
+cp -rf vim-gnupg "$BUNDLE_DIR"
+cp -rf vim-indent-guides "$BUNDLE_DIR"
+cp -rf vim-multiple-cursors "$BUNDLE_DIR"
+
