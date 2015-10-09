55d28d584bf7761d9ec615d4b3c71481dd4c1806
Darren <darren.q@gmail.com>
2015-10-09 00:18:45 -0400

    dotfiles windows script overwrites old

--- dotfiles_win.sh
+++ dotfiles_win.sh
@@ -4,16 +4,19 @@ set -x
 
 # dotfiles/.bash_aliases "grep --color" currently screws some scripts up in Windows
 cd ./dotfiles/
-cp -f .bashrc $HOME
-cp -f .bash_ssh_agent $HOME
-cp -f .gitconfig $HOME
-sed -i 's/#\(.* # Windows\)/\1/g' $HOME/.gitconfig
-cp -f .gitignore $HOME
-cp -f .vimrc $HOME
-cp -rf .vim $HOME/vimfiles
+cp -f .bashrc "$HOME"
+cp -f .bash_ssh_agent "$HOME"
+cp -f .gitconfig "$HOME"
+sed -i 's/#\(.* # Windows\)/\1/g' "$HOME/.gitconfig"
+cp -f .gitignore "$HOME"
+cp -f .vimrc "$HOME"
+
+if [ -d "$HOME/vimfiles" ]; then rm -rf "$HOME/vimfiles"; fi
+mkdir "$HOME/vimfiles"
+cp -rf .vim/* "$HOME/vimfiles/"
 
 cd ../../vim-plugins/bundle
-cp -rvf vim-pathogen/autoload $HOME/vimfiles/
+cp -rf vim-pathogen/autoload $HOME/vimfiles/
 
 BUNDLE_DIR="$HOME/vimfiles/bundle"
 mkdir "$BUNDLE_DIR"
